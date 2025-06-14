e88faa5bbd269ceee24b667947fa0ffce44b5cd9
oussamasine07 <sineoussama@gmail.com>
2025-06-14 21:08:05 +0100

    make order model

---
+++ backend/src/main/java/com/sophinia/backend/model/Order.java
@@ -0,0 +1,64 @@
+package com.sophinia.backend.model;
+
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "orders")
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "order_date")
+    private LocalDate orderDate;
+
+    @Column(name = "delivery_date")
+    private LocalDate deliveryDate;
+
+    public Order () {}
+
+    // TODO add relationships
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDate getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(LocalDate orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public LocalDate getDeliveryDate() {
+        return deliveryDate;
+    }
+
+    public void setDeliveryDate(LocalDate deliveryDate) {
+        this.deliveryDate = deliveryDate;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
