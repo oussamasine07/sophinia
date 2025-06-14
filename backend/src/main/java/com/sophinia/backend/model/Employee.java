cdbf222e973080a036d780a22c2a88f1a63e49c3
oussamasine07 <sineoussama@gmail.com>
2025-06-14 20:47:35 +0100

    make employee model

---
+++ backend/src/main/java/com/sophinia/backend/model/Employee.java
@@ -0,0 +1,98 @@
+package com.sophinia.backend.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "employees")
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "phone")
+    private String phone;
+
+    @Column(name = "address")
+    private String address;
+
+    @Column(name = "city")
+    private String city;
+
+    @Column(name = "post_code")
+    private int postalCode;
+
+    @Column(name = "cnss_code")
+    private int cnssCode;
+
+    public Employee () {}
+
+    // TODO: add relationships
+
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public int getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(int postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public int getCnssCode() {
+        return cnssCode;
+    }
+
+    public void setCnssCode(int cnssCode) {
+        this.cnssCode = cnssCode;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
