8f520c40363562392211e96f68bc0855b6ad6ede
oussamasine07 <sineoussama@gmail.com>
2025-06-14 20:55:21 +0100

    make getters and setters for appointment

--- backend/src/main/java/com/sophinia/backend/model/Appointment.java
+++ backend/src/main/java/com/sophinia/backend/model/Appointment.java
@@ -15,4 +15,19 @@ public class Appointment {
     @Column(name = "appointment_date")
     private LocalDate appointmentDate;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDate getAppointmentDate() {
+        return appointmentDate;
+    }
+
+    public void setAppointmentDate(LocalDate appointmentDate) {
+        this.appointmentDate = appointmentDate;
+    }
 }
